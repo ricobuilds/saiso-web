a5aafdffc9709d021051fabddfe30c12ad9a0b97
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-11-03 19:37:04 +0000

    added Header component

---
+++ components/Header/index.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+    return (
+        <div className="h-14 shadow-sm w-full flex justify-center items-center">
+            <span className="font-bold">SAISO</span>
+        </div>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { Header } from '../components/Header'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
@@ -11,7 +12,7 @@ const Home: NextPage = () => {
         <meta name="description" content="Saiso - a low-code feedback widget for quick feedback gathering powered by Notion." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <Header/>
       <div className="wrapper">
         <main className='main'></main>
      </div>
