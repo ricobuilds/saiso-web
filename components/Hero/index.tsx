fb0d3dbc5e81ce1377da0d4f51ead6d002cc47dc
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-11-04 09:28:08 +0000

    added notion SVG icon, modded styles and rui system

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -4,10 +4,9 @@ export const Hero = () => {
     return (
         <div className="flex flex-col items-center text-center gap-6 w-full">
             <h1 className="text-m-hero sm:text-d-hero"><span className="gradientBg bg-clip-text text-transparent">Low-Code Feedback Widget</span> Powered by Notion.</h1>
-            <p className="px-4 text-lg font-normal max-w-3xl">A plug & play feedback widget to gather feedback from your early adopters and site visitors. Better development. Focused Iterations. Great User Satisfaction.</p>
-            <div className="flex ">
-                <a href={'#setup'}><button className="py-1 px-3 h-11 rounded-md duration-200 box-border border border-transparent hover:border-rubellite-600 bg-rubellite-500 hover:bg-transparent hover:text-rubellite-600 font-semibold">Setup Instructions</button></a>
-                <button className="py-1 px-3 h-11 rounded-md duration-200 box-border border border-transparent hover:border-blue-600 bg-blue-500 hover:bg-transparent hover:text-blue-600 font-semibold">Setup Instructions</button>
+            <p className="px-4 text-d-base font-normal max-w-3xl">A plug & play feedback widget to gather feedback from your early adopters and site visitors. Better development. Focused iterations. Great user satisfaction.</p>
+            <div className="flex">
+                <a href={'#setup'} className="py-1 px-3 h-11 rounded-md duration-200 flex items-center box-border border border-transparent hover:border-ruby-600 bg-ruby-500 hover:bg-transparent hover:text-ruby-600 font-semibold outline-none">Get Started</a>
             </div>
         </div>
     )
