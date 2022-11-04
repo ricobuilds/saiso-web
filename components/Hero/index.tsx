9ffba7df82feb10f53a2f3d4c64d713be3b84e55
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-11-04 10:51:37 +0000

    updated CTA label

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -4,7 +4,7 @@ export const Hero = () => {
             <h1 className="text-m-hero sm:text-d-hero"><span className="gradientBg bg-clip-text text-transparent">The Low-Code Feedback Widget</span> Powered by Notion.</h1>
             <p className="px-4 text-d-base font-normal max-w-3xl">A plug & play feedback widget to gather feedback from your early adopters and site visitors. Better development. Focused iterations. Great user satisfaction.</p>
             <div className="flex">
-                <a href={'#setup'} className="py-1 px-3 h-11 rounded-md duration-200 flex items-center box-border border border-transparent hover:border-ruby-600 bg-ruby-500 hover:bg-transparent hover:text-ruby-600 font-semibold outline-none">Get Started Now</a>
+                <a href={'#setup'} className="py-1 px-3 h-11 rounded-md duration-200 flex items-center box-border border border-transparent hover:border-ruby-600 bg-ruby-500 hover:bg-transparent hover:text-ruby-600 font-semibold outline-none">Get started now - it's free</a>
             </div>
         </div>
     )
