bfcbc180e96f0236b67902b40073340228f3187d
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-11-09 20:15:57 +0000

    added default response if the request received does not match API structure

--- pages/api/saiso.ts
+++ pages/api/saiso.ts
@@ -1,5 +1,14 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { Client, LogLevel } from '@notionhq/client'
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {}
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    switch (req.method) {
+        case "GET":
+            //
+        case "POST":
+    
+        default:
+            return {saiso: "Caramba! It seems we got a weird request here. Try again with plain GET or POST requests!"};
+    }
+}
 
