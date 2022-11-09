f14e129d226a7796e2ba75115d72a3d3ded26544
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-11-09 20:11:26 +0000

    added handler husk

--- pages/api/saiso.ts
+++ pages/api/saiso.ts
@@ -1,3 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import {Client, LogLevel} from '@notionhq/client'
+import { Client, LogLevel } from '@notionhq/client'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {}
 
