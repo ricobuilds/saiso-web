5323f07772cd4f206e268af28ab4f607eff2fb5e
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-11-10 15:17:56 +0000

    modded responsiveness of site for small screens like apple XR

--- components/ProCard/index.tsx
+++ components/ProCard/index.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react"
 
 export const ProCardWrapper = ({ children }: { children: ReactNode }) => {
     return (
-        <div className="main h-[80vh] sm:h-[60vh]">
+        <div className="main">
             <div className="grid w-full gap-8 mx-auto sm:grid-cols-2">
                 {children}
             </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -34,13 +34,13 @@ const Home: NextPage = () => {
       <div className="fixed z-0 -right-[5%] top-[72%] sm:top-[52%] w-72 h-72 blur-[3px] opacity-40 rotate-[66deg]">
         <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500" width="100%" id="blobSvg"><defs><linearGradient id="gradient2" x1="0%" y1="0%" x2="0%" y2="100%"><stop offset="0%" style={{ stopColor: 'rgb(238, 165, 236)' }}></stop><stop offset="100%" style={{ stopColor: 'rgb(101, 91, 255)' }}></stop></linearGradient></defs><path id="blob" fill="url(#gradient2)"><animate attributeName="d" dur="5000ms" repeatCount="indefinite" values="M440.5,320.5Q418,391,355.5,442.5Q293,494,226,450.5Q159,407,99,367Q39,327,31.5,247.5Q24,168,89,125.5Q154,83,219.5,68Q285,53,335.5,94.5Q386,136,424.5,193Q463,250,440.5,320.5Z;M453.78747,319.98894Q416.97789,389.97789,353.96683,436.87838Q290.95577,483.77887,223.95577,447.43366Q156.95577,411.08845,105.64373,365.97789Q54.33169,320.86732,62.67444,252.61056Q71.01719,184.3538,113.01965,135.21007Q155.02211,86.06634,220.52211,66.46683Q286.02211,46.86732,335.5,91.94472Q384.97789,137.02211,437.78747,193.51106Q490.59704,250,453.78747,319.98894Z;M411.39826,313.90633Q402.59677,377.81265,342.92059,407.63957Q283.24442,437.46649,215.13648,432.5428Q147.02853,427.61911,82.23325,380.9572Q17.43796,334.29529,20.45223,250.83809Q23.46649,167.38089,82.5856,115.05707Q141.70471,62.73325,212.19045,63.73015Q282.67618,64.72705,352.67308,84.79839Q422.66998,104.86972,421.43486,177.43486Q420.19974,250,411.39826,313.90633Z;M440.5,320.5Q418,391,355.5,442.5Q293,494,226,450.5Q159,407,99,367Q39,327,31.5,247.5Q24,168,89,125.5Q154,83,219.5,68Q285,53,335.5,94.5Q386,136,424.5,193Q463,250,440.5,320.5Z;"></animate></path></svg>
       </div>
-      <div className="h-[86vh] w-full wrapper">
+      <div className="w-full h-fit wrapper ">
         <Header />
         <div className='pt-24 main'>
           <Hero />
         </div>
       </div>
-      <div id='problem' className="main justify-center sm:justify-start gap-4 h-[60vh] my-12 mb-24">
+      <div id='problem' className="justify-center gap-4 my-24 mt-52 main sm:justify-start h-fit">
         <h2 className='font-medium text-center text-m-h2 sm:text-d-h2'>How modern solopreneurs & small teams create <span className='text-center text-transparent gradientBg bg-clip-text'>product-led solutions</span></h2>
         <p className='text-center text-d-base'>
           Saiso <span className='text-rosian-400'>(pronounced &apos;Say-So&apos;)</span> is a feedback widget and API powered by Notion
@@ -69,7 +69,7 @@ const Home: NextPage = () => {
       <div id="setup" className="my-24 main">
         <h2 className='font-medium text-center text-m-h2 sm:text-d-h2'>Setting up <span className='text-rosian-500'>Saiso</span></h2>
         <p className='text-center'>Implement Saiso&apos;s widget in 5 simple steps.</p>
-        <div className="mt-12 space-y-24">
+        <div className="my-12 space-y-24">
           <div className="flex flex-col items-center justify-center gap-8 sm:flex-row">
             {/* setup notion DB */}
             <div className="z-10 text-center">
